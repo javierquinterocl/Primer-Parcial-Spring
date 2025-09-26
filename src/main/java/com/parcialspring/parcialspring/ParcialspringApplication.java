e1bf012f49ee7eb7b22ff0223336a77d11b21b7d
javierquinterocl <jaquinterocl@ufpso.edu.co>
2025-09-25 19:30:21 -0500

    base proyect

---
+++ src/main/java/com/parcialspring/parcialspring/ParcialspringApplication.java
@@ -0,0 +1,13 @@
+package com.parcialspring.parcialspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcialspring/parcialspring/ParcialspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcialspring.parcialspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
